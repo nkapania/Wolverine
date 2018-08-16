614d74ca39156ae0577a6dc080d48f16a690a7ab
Nitin <nkapania@gmail.com>
2018-08-16 16:57:24 -0700

    Starting to put in the map match code from shelley. will need to discuss if this is ok to share.

--- utils/velocityprofiles.py
+++ utils/velocityprofiles.py
@@ -18,8 +18,108 @@ class VelocityProfile:
 			else:
 				self.s, self.Ax, self.Ux = generateRacingProfileClosed(vehicle, path, friction, vMax)
 
+		if self.type is "racingSubosits":
+			self.s, self.Ax, self.ux = generateRacingProfileSubosits(vehicle, path, friction, vMax)
+
+
 		else:
 			print("Error")
+
+
+
+#Uses Subosits and Mick's racing controller accounting for bank and grade, brake factor, weight transfer. 
+def generateRacingProfileSubosits(vehicle, path, friction, vMax, jerkLimit = 800, ds = 0.5):
+	#Jerk limit in 800 m/s^3, how fast car an come off the brakes
+	topoData = np.zeros((2,7))
+	topData[1,0] = path.s[-1] #just create dummy data - no need to simulate bank and grade here
+
+
+	s, Fv2, G, Mv2, Mvdot, theta = makePath3D(path, topodata, vehicle, ds)
+
+
+
+# Combines the two-dimensional path data with the topography data
+#to make the path 3D.
+#   mapData is the map read in according to InitMap.m, topoData is topograghy
+#   data as used in InitSpeedProfile.m, and ds is the desired spacing
+#   between computation points.  s is a vector of distances along the path
+#   that indexes the other outputs.  
+
+def makePath3D(path, topodata, vehicle, ds):
+	L = path.s[-1]
+	numPoints = floor(L / ds)
+	s = np.linspace(0, L, numPoints)
+
+	#grade info wrt s
+	theta = np.interp(s, topoData[:,0], topoData[:,1])
+	dtheta_ds = np.interp(s, topoData[:,0], topoData[:,2])
+	d2theta_ds = np.interp(s, topoData[:,0], topoData[:,3]) 
+
+	#bank info wrt s
+	phi = np.interp(s, topoData[:,0], topoData[:,4])
+	dphi_ds = np.interp(s, topoData[:,0], topoData[:,5])
+	d2phi_ds2 = np.interp(s, topoData[:,0], topoData[:,6])
+
+	#heading info wrt s
+	psi = np.interp(s, path.s, path.roadPsi)
+	dpsi_ds = np.interp(s, path.s, path.K)
+	dpsi_ds2 = np.divide( np.diff(dpsi_ds) , np.diff(s) )
+	dpsi_ds2 = np.insert(dpsi_ds2, 0, 0) #prepend zero to keep same size
+
+	#find coefficients for the forces
+	Fv2, G = _calcderivatives(psi, theta, phi, dpsi_ds, dtheta_ds)
+
+	#find coefficients for the moments
+	Ib = np.diag( np.array([0.1 * veh.*Iz 0.8*veh.Iz veh.Iz]) )
+	[Mv2, Mvdot] = _rotaryderivatives(Ib, theta, phi, dpsi_ds, dtheta_ds, dphi_ds, d2psi_ds2, d2theta_ds2, d2phi_ds2)
+
+	return s, Fv2, G, Mv2, Mvdot, theta
+
+
+
+
+    # %CalcDerivatives returns vectors for acclerations in the body frame
+    # %   Fv2 is the pointwise acceleration from curvature. Fvdot is the
+    # %   acceleration from gas/brake and is [1;0;0] in every column.  G is the
+    # %   direction of gravity in the body frame (positive down).  
+
+
+def _calcderivatives(psi, theta, phi, dpsi, dtheta):
+	N = len(psi)
+
+	#path derivatives
+	dx = np.dot (np.cos(theta) , np.cos(psi) )
+	dy = np.dot (np.cos(theta) , np.sin(psi) )
+	dz = -np.sin(theta)
+
+    dx2_ds2 = - np.multiply( np.sin(theta), np.cos(theta), np.cos(psi), np.dtheta_dsigma)  - np.multiply(np.cos(theta), np.cos(theta) , np.sin(psi), dpsi)
+    dy2_ds2 =   np.multiply( np.cos(theta), np.cos(theta), np.cos(psi), dpsi_dsigma     )  - np.multiply(np.sin(theta), np.cos(theta) , np.sin(psi), dtheta);
+    dz2_ds2 = - np.multiply(np.cos(theta), np.cos(theta), dtheta_dsigma)
+
+    #initialize output variables
+    Fv2 = np.zeros(3, N)
+    G = zeros(3, N)
+
+    #define transformation matrix M_BI
+
+    for i in range(n):
+    	M1 = np.array([[ np.cos(psi[i]),   np.sin(psi[i]), 0]  , [-np.sin(psi[i]), np.cos(psi[i]), 0 ],  [0, 0, 1]])
+    	M2 = np.array  (  [[np.cos(theta[i]) , 0 , -sin(theta[i])]  , [0, 1, 0], [np.sin(theta[i]), 0, np.cos(theta[i]) ]] ) 
+    	MB2 = np.array (  [[1, 0, 0]  , [0, np.cos(phi[i]), np.sin(phi[i]) ],[ 0, -sin(phi[i]), cos(phi[i])] ])
+    	
+    	MBI  = np.matmul ( np.matmul ( MB2, M2) , M1)
+
+    	Fv2[:,i] = np.dot(MBI , np.array([[dx2[i]],[dy2[i]],[dz2[i]]]) 
+    	G[:,i]   - np.dot(MBI , np.array([0],[0],[9.81]))
+
+    	return Fv2, G
+
+
+
+
+
+
+
 
 
 
